ac253c05f96613dc48fc3d9faa2b36d7e6d58ab2
Jerric1801 <Jerric1801@gmail.com>
2024-10-13 21:58:58 +0800

    health check config for load balancer

--- src/server.ts
+++ src/server.ts
@@ -24,6 +24,10 @@ app.use('/api/view-schedule', viewScheduleRoutes);
 
 // Health check route
 app.get('/health', (req, res) => {
+    res.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+    res.set('Pragma', 'no-cache'); 
+    res.set('Expires', '0');
+    res.set('Surrogate-Control', 'no-store');   
     res.status(200).json({ status: 'ok', message: 'Server is running!' });
 });
 
